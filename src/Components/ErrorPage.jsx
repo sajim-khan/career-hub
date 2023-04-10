433b4ec46318245abef37caebce7bc4de74005c1
sajim-khan <shossain1427@gmail.com>
2023-04-10 15:56:34 +0600

    added showing error page message when get an error

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Error</h1>
+    
+  const { error, status } = useRouteError();
+  //console.log(error.message);
+  return (
+    <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
+      <div className=" flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-7xl text-gray-700">
+            <span className="sr-only">Error</span> {status}
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl mb-8">
+            {error?.message}
+          </p>
+          <Link
+            to="/"
+            className="px-8 py-3 font-semibold rounded bg-cyan-700 text-gray-300"
+          >
+            Back to homepage
+          </Link>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default ErrorPage;
