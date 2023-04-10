02eae4c4733f61b679981c26f3dbd215187a201b
sajim-khan <shossain1427@gmail.com>
2023-04-10 16:15:08 +0600

    added footer section

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -9,15 +9,16 @@ const ErrorPage = () => {
     <section className="flex items-center h-screen p-16 bg-gray-100 text-gray-900">
       <div className=" flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-7xl text-gray-700">
-            <span className="sr-only">Error</span> {status}
+          <h1 className="mb-4 font-extrabold text-5xl text-gray-700">Opps!!</h1>
+          <h2 className="mb-8 font-extrabold text-6xl text-gray-700">
+            <span className="sr-only">Error</span> Error {status}
           </h2>
           <p className="text-2xl font-semibold md:text-3xl mb-8">
             {error?.message}
           </p>
           <Link
             to="/"
-            className="px-8 py-3 font-semibold rounded bg-cyan-700 text-gray-300"
+            className="px-8 py-3 font-semibold rounded bg-cyan-700 text-gray-100"
           >
             Back to homepage
           </Link>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
+      <footer className="flex space-x-7  items-center justify-center space-between text-gray-800">
+        <div className="py-6 text-sm text-center text-black">
+          © 2023 JobSpot. All rights reserved.
         </div>
+        <div className="py-6 text-sm text-center text-black">
+          Powered by CareerHub
+        </div>
+      </footer>
     );
 };
 
